62ee2004a81a7645c9b78a8fe8dfe78b0473a000
tiantsoash98 <tiantsoa.sh@gmail.com>
2023-10-21 20:38:25 +0300

    Footer

--- src/features/smoothScroll.js
+++ src/features/smoothScroll.js
@@ -2,7 +2,7 @@ import Lenis from '@studio-freight/lenis'
 
 function smoothScroll() {
   const lenis = new Lenis({
-    duration: 1.5,
+    duration: 2,
     easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     direction: 'vertical',
     gestureDirection: 'vertical',
