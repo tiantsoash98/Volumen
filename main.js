0bc0a1c2bfa6d60a34ea7ad9e5b32733068bf418
tiantsoash98 <tiantsoa.sh@gmail.com>
2023-10-21 10:46:13 +0300

    Install Mouse Follower

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 import animateWhilePageLoading from './src/features/animateWhilePageLoading'
 import animateOnPageLoaded from './src/features/animateOnPageLoaded'
+import mouseFollower from './src/features/mouseFollower'
 
 animateWhilePageLoading()
 
 window.addEventListener('load', () => {
   // Run when page loaded
   setTimeout(() => animateOnPageLoaded(), 3000)
+  mouseFollower()
 })
---
+++ src/features/mouseFollower.js
@@ -0,0 +1,16 @@
+import MouseFollower from "mouse-follower";
+import gsap from "gsap";
+
+
+
+function mouseFollower(){
+    MouseFollower.registerGSAP(gsap);
+
+    const cursor = new MouseFollower({
+        container: document.body,
+        speed: 1,
+        ease: 'expo.out',
+    });
+}
+
+export default mouseFollower
