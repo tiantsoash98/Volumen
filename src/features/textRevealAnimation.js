c8f78d5dee24a591dea123660c6342288343f74f
tiantsoash98 <tiantsoa.sh@gmail.com>
2023-10-26 20:18:56 +0300

    Responsive header
    CSS responsive
    Animation off on mobile

--- src/features/textRevealAnimation.js
+++ src/features/textRevealAnimation.js
@@ -4,25 +4,28 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
 function textRevealAnimation(){
+    let matchMedia = gsap.matchMedia();
     gsap.registerPlugin(ScrollTrigger)
 
     $(".text-reveal").each(function (index) {
         let triggerElement = $(this);
         let targetElement = $(this);
-    
-        let tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: triggerElement,
-                //trigger element - viewport
-                start: "top 90%",
-                end: "top center",
-                scrub: true,
-            }
+
+        matchMedia.add("(min-width: 768px)", () => {
+            let tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: triggerElement,
+                    //trigger element - viewport
+                    start: "top 90%",
+                    end: "top center",
+                    scrub: true,
+                }
+            });
+            tl.from(targetElement, {
+                opacity: 0.1,
+                duration: 1
+            })
         });
-        tl.from(targetElement, {
-            opacity: 0.1,
-            duration: 1
-        })
     });
 }
 
