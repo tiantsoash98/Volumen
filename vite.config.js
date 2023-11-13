680cadb8b67a431268615c86a9c6dd2f98d34284
tiantsoash98 <tiantsoa.sh@gmail.com>
2023-11-13 15:08:42 +0300

    Multi-page App config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sphere: resolve(__dirname, 'sphere/index.html'),
+      },
+    },
+  },
+})
