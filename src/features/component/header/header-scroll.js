b4492454fc60b7030ab7d101db81246199e24f5e
tiantsoash98 <tiantsoa.sh@gmail.com>
2023-11-13 17:31:03 +0300

    Rendered Images

--- src/features/component/header/header-scroll.js
+++ src/features/component/header/header-scroll.js
@@ -2,24 +2,25 @@ import $ from "jquery";
 
 function headerScroll()
 {
-    let currentScroll = 0;
-  
-    $(window).on('scroll',function() {
-      var targetElement = $('.header');
-      var starthidePosition = targetElement.position().top + $(this).height();
+  var targetElement = $('.header');
+  let currentScroll = 0;
 
-      // hide navbar on scroll
-      if($(this).scrollTop() > starthidePosition) { 
-        if(currentScroll < $(this).scrollTop()){
-            targetElement.addClass('header--scrolling');
-        }
-        else{
-            targetElement.removeClass('header--scrolling');
-        }
+  $(window).on('scroll',function() {
+    
+    var starthidePosition = targetElement.position().top + $(this).height()/2;
+
+    // hide navbar on scroll
+    if($(this).scrollTop() > starthidePosition) { 
+      if(currentScroll < $(this).scrollTop()){
+          targetElement.addClass('header--scrolling');
       }
-  
-      currentScroll = $(this).scrollTop();
-    });
+      else{
+          targetElement.removeClass('header--scrolling');
+      }
+    }
+
+    currentScroll = $(this).scrollTop();
+  });
 }
 
 export default headerScroll
