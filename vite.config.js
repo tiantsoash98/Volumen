09c845a60f0bc37cc02463edace1126a69fdb1c1
tiantsoash98 <tiantsoa.sh@gmail.com>
2023-11-18 21:34:29 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,30 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: {
+    host: 'localhost',
+    cors: '*',
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+    },
+  },
   build: {
+    minify: true,
+    manifest: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        sphere: resolve(__dirname, 'sphere/index.html'),
+        // sphere: resolve(__dirname, 'sphere/index.html'),
+      },
+      output: {
+        format: 'umd',
+        entryFileNames: 'main.js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
       },
     },
   },
