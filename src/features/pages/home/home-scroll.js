c2bd9be02a187c6c69764b631dfb120f63f6f05d
tiantsoash98 <tiantsoa.sh@gmail.com>
2023-11-21 19:47:10 +0300

    Home text reveal

--- src/features/pages/home/home-scroll.js
+++ src/features/pages/home/home-scroll.js
@@ -1,8 +1,36 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import SplitType from "split-type";
 
 function homeScroll(){
     gsap.registerPlugin(ScrollTrigger);
+
+
+    SplitType.create('.hero__content', {types: 'lines, words', lineClass: 'hero__content--line', wordClass: "hero__content--word"});
+    
+    let delay = 0
+    let heroLines = gsap.utils.toArray('.hero__content--line')
+
+    heroLines.forEach((heroLine) => {
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: ".hero__content",
+                //trigger element - viewport
+                start: "top 80%",
+                end: "top 40%",
+            }
+        })
+        .from(heroLine.querySelectorAll('.hero__content--word'), {
+            opacity: 0,
+            yPercent: 100,
+            duration: 0.8,
+            delay: delay
+        })
+
+        delay+=0.1
+    })
+
+    
 
     let showNavMenuBreakpoint = getComputedStyle(document.body).getPropertyValue('--default-show-nav-menu-breakpoint');
     let matchMedia = gsap.matchMedia();
