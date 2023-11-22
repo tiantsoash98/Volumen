565ff29d1211079169895d6de28c4eba2b33dafa
tiantsoash98 <tiantsoa.sh@gmail.com>
2023-11-22 11:52:12 +0300

    Repertoire hover

--- src/features/component/repertoire/Repertoire.vue
+++ src/features/component/repertoire/Repertoire.vue
@@ -35,15 +35,18 @@ export default {
             <article v-for="(shape, index) in shapes" :key="shape.id" :class="`repertoire__item repertoire__item--${index+1}`" >
                 <figure class="repertoire__image-wrapper image-scroll__wrapper">
                     <a :href="getShapeHref(shape.slug)">
-                        <img class="repertoire__img repertoire__img--main image-scroll" :src="getImgByOrientation(shape.slug, shape.repertoire.preferedOrientation, shape.repertoire.imgId)" :alt="shape.name" loading="lazy">
+                        <img class="repertoire__img repertoire__img--main image-scroll" :src="getImgByOrientation(shape.slug, shape.repertoire.preferedOrientation, shape.repertoire.imgId)" :alt="shape.name" loading="eager">
                         <img class="repertoire__img repertoire__img--hover" :src="getImgByOrientation(shape.slug, shape.repertoire.preferedOrientation, shape.repertoire.hoverId)" :alt="shape.name" loading="lazy">
                     </a>
                 </figure>
                 <div class="repertoire__details">
-                    <div class="repertoire__title">
-                        <a :href="getShapeHref(shape.slug)">{{ shape.name }}</a>
+                    <div class="repertoire__title-wrapper">
+                        <a :href="getShapeHref(shape.slug)">
+                            <div class="repertoire__title repertoire__title--name">{{ shape.name }}</div>
+                            <div class="repertoire__title repertoire__title--description">{{ shape.subtitle }}</div>
+                        </a>
                     </div>
-                    <div class="repertoire__year">
+                    <div class="repertoire__view">
                         <a :href="getShapeHref(shape.slug)" class="hover-link">View</a>
                     </div>
                 </div>
--- src/features/component/repertoire/repertoire.js
+++ src/features/component/repertoire/repertoire.js
@@ -8,20 +8,24 @@ function repertoire(){
     createApp(Repertoire).mount('#repertoire');
 
     let defaultEase = getComputedStyle(document.body).getPropertyValue('--default-ease');
-
     gsap.registerPlugin(CustomEase) 
     CustomEase.create("customEase", defaultEase);
 
-
+    
     // Repertoire item hover
+    
     let items = gsap.utils.toArray('.repertoire__item')
 
     items.forEach((item) => {
         // Hover in
         $(item).on('mouseenter', () => {
             gsap.timeline({
-                defaults: { ease: "customEase" },
+                defaults: { 
+                    duration: 1,
+                    ease: "customEase" 
+                },
             })
+            // Image animation
             .to(item.querySelector('.repertoire__img--main'), {
                 opacity: 0,
                 duration: 1,
@@ -31,16 +35,35 @@ function repertoire(){
                 scale: 1.2,
                 duration: 2
             }, '<')
+            // Title animation
+            .to(item.querySelector('.repertoire__title--name'), {
+                yPercent: -100,
+                duration: 1.3,
+            }, '<')
+            .to(item.querySelector('.repertoire__title--description'), {
+                yPercent: -100,
+                opacity: 1,
+                duration: 1.3,
+            }, '<')
         })
 
         // Hover out
         $(item).on('mouseleave', () => {
             gsap.timeline({
-                defaults: { duration: 1, ease: "customEase" },
+                defaults: { 
+                    duration: 1, 
+                    ease: "customEase" },
             })
             .to(item.querySelector('.repertoire__img--main'), {
                 opacity: 1
             })
+            .to(item.querySelector('.repertoire__title--description'), {
+                yPercent: 0,
+                opacity: 0
+            }, '<')
+            .to(item.querySelector('.repertoire__title--name'), {
+                yPercent: 0
+            }, '<')
             .set(item.querySelector('.repertoire__img--hover'), {
                 scale: 1
             })
